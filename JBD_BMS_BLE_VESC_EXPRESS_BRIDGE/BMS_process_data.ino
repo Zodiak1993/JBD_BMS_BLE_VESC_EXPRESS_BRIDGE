7fcaa869cea9e3a6cd1e316d7410775cf788fb49
Zodiak1993 <96478454+Zodiak1993@users.noreply.github.com>
2025-11-01 20:29:53 +0100

    Update BMS_process_data.ino

--- JBD_BMS_BLE_VESC_EXPRESS_BRIDGE/BMS_process_data.ino
+++ JBD_BMS_BLE_VESC_EXPRESS_BRIDGE/BMS_process_data.ino
@@ -1,68 +1,183 @@
-bool isPacketValid(byte *packet)  //check if packet is valid
-{
-  TRACE;
-  if (packet == nullptr) {
+bool isPacketValid(byte *packet, int packetLength) {
+  if (packetLength < 6) { // Minimum: Start + Cmd + Status + dataLen + 2 Checksum + End
+    commSerial.println("❌ Packet too short!");
     return false;
   }
 
-  bmsPacketHeaderStruct *pHeader = (bmsPacketHeaderStruct *)packet;
-  int checksumLen = pHeader->dataLen + 2;  // status + data len + data
-
-  if (pHeader->start != 0xDD) {
+  // Start-Byte prüfen
+  if (packet[0] != 0xDD) {
+    commSerial.printf("❌ Invalid start byte! Got 0x%02X\n", packet[0]);
     return false;
   }
 
-  int offset = 2;  // header 0xDD and command type are skipped
+  // Datenlänge aus Header
+  uint8_t dataLen = packet[3];
+  int expectedLen = 4 + dataLen + 3; // Start + Cmd + Status + dataLen + 2 Checksum + End
 
-  byte checksum = 0;
-  for (int i = 0; i < checksumLen; i++) {
-    checksum += packet[offset + i];
-  }
-
-  //printf("checksum: %x\n", checksum);
-
-  checksum = ((checksum ^ 0xFF) + 1) & 0xFF;
-  //printf("checksum v2: %x\n", checksum);
-
-  byte rxChecksum = packet[offset + checksumLen + 1];
-
-  if (checksum == rxChecksum) {
-    //printf("Packet is valid\n");
-    return true;
-  } else {
-    //printf("Packet is not valid\n");
-    //printf("Expected value: %x\n", rxChecksum);
+  if (packetLength != expectedLen) {
+    commSerial.printf("❌ Packet length mismatch! Got %d, expected %d\n", packetLength, expectedLen);
     return false;
   }
+
+  // End-Byte prüfen
+  if (packet[packetLength - 1] != 0x77) {
+    commSerial.printf("❌ Invalid end byte 0x%02X (expected 0x77)\n", packet[packetLength - 1]);
+    return false;
+  }
+
+  // 16-Bit Checksumme berechnen (Status + alle Datenbytes)
+  uint16_t calcChecksum = 0;
+  // Status = packet[2], Daten = packet[4..4+dataLen-1]
+  for (int i = 2; i < 4 + dataLen; i++) {
+    calcChecksum += packet[i];
+  }
+  calcChecksum = (~calcChecksum + 1) & 0xFFFF; // Two's complement 16-bit
+
+  // Empfangene Checksumme aus Paket
+  uint8_t rxHigh = packet[4 + dataLen];      // High Byte
+  uint8_t rxLow  = packet[4 + dataLen + 1];  // Low Byte
+  uint16_t rxChecksum = ((uint16_t)rxHigh << 8) | rxLow;
+
+  commSerial.printf("Checksum (recv=0x%04X calc=0x%04X)\n", rxChecksum, calcChecksum);
+
+  if (rxChecksum != calcChecksum) {
+    commSerial.println("❌ Invalid checksum");
+    return false;
+  }
+
+  return true;
 }
+
+
+
+
+
+
+bool bmsProcessPacket(byte *packet) {
+    TRACE;
+
+    bmsPacketHeaderStruct *pHeader = (bmsPacketHeaderStruct *)packet;
+    byte *data = packet + sizeof(bmsPacketHeaderStruct);
+    unsigned int dataLen = pHeader->dataLen;
+
+    int packetLength = 4 + dataLen + 2 + 1; // Start+Cmd+Status+dataLen + 2 Checksum + Endbyte
+
+    if (!isPacketValid(packet, packetLength)) {
+        commSerial.println("❌ Invalid packet received");
+        return false;
+    }
+
+    bool result = false;
+
+    switch (pHeader->type) {
+        case cBasicInfo3:
+            //commSerial.println("BasicInfo3");
+            result = processBasicInfo(&packBasicInfo, data, dataLen);
+            newPacketReceived = true;
+            break;
+
+        case cCellInfo4:
+            result = processCellInfo(&packCellInfo, data, dataLen);
+            newPacketReceived = true;
+            break;
+
+        default:
+            commSerial.printf("Unsupported packet type detected. Type: %d\n", pHeader->type);
+            result = false;
+    }
+
+    return result;
+}
+
+
+
+
+
+bool bleCollectPacket(char *data, uint32_t dataSize) {
+    TRACE;
+
+    static uint8_t packetstate = 0;
+    static uint8_t packetbuff[50] = {0};
+    static uint32_t bufferLen = 0;
+    bool retVal = false;
+
+    // Debug Eingang
+    //commSerial.printf("DEBUG: Received %lu bytes\n", dataSize);
+    //for (uint32_t i = 0; i < dataSize; i++) commSerial.printf("%02X ", (uint8_t)data[i]);
+    //commSerial.println("\n------------------------------------------");
+
+    // --- Wenn neues Paket anfängt ---
+    if (data[0] == 0xDD) {
+        //commSerial.println("bleCollectPacket_1_start");
+        packetstate = 1;
+        bufferLen = 0;
+    }
+
+    // --- Daten IMMER anhängen ---
+    for (uint32_t i = 0; i < dataSize; i++) {
+        packetbuff[bufferLen + i] = data[i];
+    }
+    bufferLen += dataSize;
+
+    //commSerial.printf("DEBUG: Appended %lu bytes, buffer length = %lu\n", dataSize, bufferLen);
+
+    // --- Prüfen, ob 0x77 am Ende ---
+    if (data[dataSize - 1] == 0x77 && packetstate == 1) {
+        //commSerial.println("bleCollectPacket_2_ende");
+
+        // Debug Dump vor Verarbeitung
+        commSerial.printf("Total length = %lu bytes\n", bufferLen);
+        commSerial.printf("🔍Final buffer dump before validation:\n");
+        for (uint32_t i = 0; i < bufferLen; i++) commSerial.printf("%02X ", packetbuff[i]);
+        commSerial.printf("\n");
+        // Jetzt vollständiges Paket verarbeiten
+        bmsProcessPacket(packetbuff);
+
+        // Reset
+        packetstate = 0;
+        bufferLen = 0;
+        memset(packetbuff, 0, sizeof(packetbuff));
+        retVal = true;
+    }
+
+    return retVal;
+}
+
+
+
+
+
 
 bool processBasicInfo(packBasicInfoStruct *output, byte *data, unsigned int dataLen) {
   TRACE;
   // Expected data len
-  if (dataLen != 0x1B) {
+  if (dataLen != 0x26) {
+  commSerial.printf("fuck - wrong datalen");  
     return false;
   }
   // String myString = String(&data);
-  float nominalVoltage = 12.8;
+  float nominalVoltage = 57.6;
   output->Volts = ((uint32_t)two_ints_into16(data[0], data[1])) * 10;  // Resolution 10 mV -> convert to milivolts   eg 4895 > 48950mV
   output->Amps = ((int32_t)two_ints_into16(data[2], data[3])) * 10;    // Resolution 10 mA -> convert to miliamps
 
   output->Watts = output->Volts * output->Amps / 1000000;  // W
   output->FullCapacity = ((uint16_t)two_ints_into16(data[6], data[7]));
-  // output->CapacityRemainAh = ((uint16_t)two_ints_into16(data[4], data[5])) * 10;
-  output->CapacityRemainAh = ((uint16_t)output->FullCapacity * output->CapacityRemainPercent / 100);
+  output->CapacityRemainAh = ((uint16_t)two_ints_into16(data[4], data[5]));
+  output->CapacityRemainPercent = ((uint8_t)data[19]);
+  //output->CapacityRemainAh = ((uint16_t)output->FullCapacity * output->CapacityRemainPercent / 100);
 
   output->Cycles = ((uint16_t)two_ints_into16(data[8], data[9]));
 
-  output->CapacityRemainPercent = ((uint8_t)data[19]);
 
   output->CapacityRemainWh = ((uint32_t)(output->FullCapacity / 100 * (nominalVoltage * 10) * output->CapacityRemainPercent / 100));
 
   output->Temp1 = (((uint16_t)two_ints_into16(data[23], data[24])) - 2731);
   output->Temp2 = (((uint16_t)two_ints_into16(data[25], data[26])) - 2731);
+  output->Temp3 = (((uint16_t)two_ints_into16(data[27], data[28])) - 2731);
   output->BalanceCodeLow = (two_ints_into16(data[12], data[13]));
   output->BalanceCodeHigh = (two_ints_into16(data[14], data[15]));
   output->MosfetStatus = ((byte)data[20]);
+  output->BatterySeries = ((uint8_t)data[21]);
   // for (int i = 0; i < dataLen; i++) {
   //   commSerial.print(data[i]);
   // }
@@ -135,138 +250,7 @@ bool processCellInfo(packCellInfoStruct *output, byte *data, unsigned int dataLe
   return true;
 };
 
-bool bmsProcessPacket(byte *packet) {
-  TRACE;
-  bool isValid = isPacketValid(packet);
 
-  if (isValid != true) {
-    commSerial.println("Invalid packet received");
-    return false;
-  }
-
-  bmsPacketHeaderStruct *pHeader = (bmsPacketHeaderStruct *)packet;
-  byte *data = packet + sizeof(bmsPacketHeaderStruct);  // TODO Fix this ugly hack
-  unsigned int dataLen = pHeader->dataLen;
-
-  bool result = false;
-
-  // |Decision based on packet type (info3 or info4)
-  switch (pHeader->type) {
-    case cBasicInfo3:
-      {
-        // Process basic info
-        result = processBasicInfo(&packBasicInfo, data, dataLen);
-        newPacketReceived = true;
-        break;
-      }
-
-    case cCellInfo4:
-      {
-        result = processCellInfo(&packCellInfo, data, dataLen);
-        newPacketReceived = true;
-        break;
-      }
-
-    default:
-      result = false;
-      commSerial.printf("Unsupported packet type detected. Type: %d", pHeader->type);
-  }
-
-  return result;
-}
-
-// bool bmsCollectPacket_uart(byte *packet)  //unused function to get packet directly from uart
-// {
-//   TRACE;
-// #define packet1stByte 0xdd
-// #define packet2ndByte 0x03
-// #define packet2ndByte_alt 0x04
-// #define packetLastByte 0x77
-//   bool retVal;
-//   byte actualByte;
-//   static byte previousByte;
-//   static bool inProgress = false;
-//   static byte bmsPacketBuff[40];
-//   static byte bmsPacketBuffCount;
-
-//   if (bmsSerial.available() > 0)  //data in serial buffer available
-//   {
-//     actualByte = bmsSerial.read();
-//     if (previousByte == packetLastByte && actualByte == packet1stByte)  //got packet footer
-//     {
-//       memcpy(packet, bmsPacketBuff, bmsPacketBuffCount);
-//       inProgress = false;
-//       retVal = true;
-//     }
-//     if (inProgress)  //filling bytes to output buffer
-//     {
-//       bmsPacketBuff[bmsPacketBuffCount] = actualByte;
-//       bmsPacketBuffCount++;
-//       retVal = false;
-//     }
-
-//     if (previousByte == packet1stByte && (actualByte == packet2ndByte || actualByte == packet2ndByte_alt))  //got packet header
-//     {
-//       bmsPacketBuff[0] = previousByte;
-//       bmsPacketBuff[1] = actualByte;
-//       bmsPacketBuffCount = 2;  // for next pass. [0] and [1] are filled already
-//       inProgress = true;
-//       retVal = false;
-//     }
-
-//     previousByte = actualByte;
-//   } else {
-//     retVal = false;
-//   }
-//   return retVal;
-// }
-
-bool bleCollectPacket(char *data, uint32_t dataSize)  // reconstruct packet from BLE incomming data, called by notifyCallback function
-{
-  TRACE;
-  /*
-        0 - empty, 
-        1 - first half of packet received, 
-        2- second half of packet received
-    */
-  static uint8_t packetstate = 0;
-  static uint8_t packetbuff[40] = { 0x0 };
-  static uint32_t previousDataSize = 0;
-  bool retVal = false;
-  //hexDump(data,dataSize);
-  // commSerial.print("bleCollectPacket_1");
-  if (data[0] == 0xdd && packetstate == 0)  // probably got 1st half of packet
-  {
-    packetstate = 1;
-    previousDataSize = dataSize;
-    for (uint8_t i = 0; i < dataSize; i++) {
-      packetbuff[i] = data[i];
-    }
-    retVal = false;
-  }
-  // commSerial.print("bleCollectPacket_2");
-
-  if (data[dataSize - 1] == 0x77 && packetstate == 1)  //probably got 2nd half of the packet
-  {
-    packetstate = 2;
-    for (uint8_t i = 0; i < dataSize; i++) {
-      packetbuff[i + previousDataSize] = data[i];
-    }
-    retVal = false;
-  }
-  // commSerial.print("bleCollectPacket_3");
-
-  if (packetstate == 2)  //got full packet
-  {
-    uint8_t packet[dataSize + previousDataSize];
-    memcpy(packet, packetbuff, dataSize + previousDataSize);
-
-    bmsProcessPacket(packet);  //pass pointer to retrieved packet to processing function
-    packetstate = 0;
-    retVal = true;
-  }
-  return retVal;
-}
 
 void sendCommand(uint8_t *data, uint32_t dataLen) {
   //TRACE;
@@ -312,23 +296,29 @@ void bmsGetInfo4() {
 void printBasicInfo()  //debug all data to uart
 {
   TRACE;
-  commSerial.printf("Total voltage: %.2f\n", (float)packBasicInfo.Volts / 1000);
-  commSerial.printf("Amps: %.2f\n", (float)packBasicInfo.Amps / 1000);
-  commSerial.printf("CapacityRemain Ah: %.2f\n", (float)packBasicInfo.CapacityRemainAh / 100);
-  commSerial.printf("CapacityRemain %%: %.2f\n", (float)packBasicInfo.CapacityRemainPercent);
-  commSerial.printf("CapacityRemain kWh: %.2f\n", (float)packBasicInfo.CapacityRemainWh / 10000);
-  commSerial.printf("FullCapacity Ah: %.2f\n", (float)packBasicInfo.FullCapacity / 100);
+  commSerial.printf("=======================================================================================================================================\n");
+  commSerial.printf("Total voltage: %.2f V\n", (float)packBasicInfo.Volts / 1000);
+  commSerial.printf("Amps: %.2f A\n", (float)packBasicInfo.Amps / 1000);
+  commSerial.printf("Watts: %.2f W\n", (float)packBasicInfo.Watts);
+  commSerial.printf("CapacityRemain: %.2f Ah\n", (float)packBasicInfo.CapacityRemainAh / 100);
+  commSerial.printf("CapacityRemain: %.2f %%\n", (float)packBasicInfo.CapacityRemainPercent);
+  commSerial.printf("CapacityRemain: %.0f Wh\n", (float)packBasicInfo.CapacityRemainWh / 10);
+  commSerial.printf("FullCapacity: %.2f Ah\n", (float)packBasicInfo.FullCapacity / 100);
   commSerial.printf("Cycles: %d\n", packBasicInfo.Cycles);
-  commSerial.printf("Temp1: %.2f\n", (float)packBasicInfo.Temp1 / 10);
-  commSerial.printf("Temp2: %.2f\n", (float)packBasicInfo.Temp2 / 10);
+  commSerial.printf("Battery cells in series: %u\n", packBasicInfo.BatterySeries);
+  commSerial.printf("Temp1: %.2f°\n", (float)packBasicInfo.Temp1 / 10);
+  commSerial.printf("Temp2: %.2f°\n", (float)packBasicInfo.Temp2 / 10);
+  commSerial.printf("Temp3: %.2f°\n", (float)packBasicInfo.Temp3 / 10);
   commSerial.printf("Balance Code Low: 0x%x\n", packBasicInfo.BalanceCodeLow);
   commSerial.printf("Balance Code High: 0x%x\n", packBasicInfo.BalanceCodeHigh);
   commSerial.printf("Mosfet Status: 0x%x\n", packBasicInfo.MosfetStatus);
+  commSerial.printf("=======================================================================================================================================\n");
 }
 
 void printCellInfo()  //debug all data to uart
 {
   TRACE;
+  commSerial.printf("=======================================================================================================================================\n");
   commSerial.printf("Number of cells: %u\n", packCellInfo.NumOfCells);
   for (byte i = 1; i <= packCellInfo.NumOfCells; i++) {
     commSerial.printf("Cell no. %u", i);
@@ -339,7 +329,7 @@ void printCellInfo()  //debug all data to uart
   commSerial.printf("Difference cell volt: %.3f\n", (float)packCellInfo.CellDiff / 1000);
   commSerial.printf("Average cell volt: %.3f\n", (float)packCellInfo.CellAvg / 1000);
   commSerial.printf("Median cell volt: %.3f\n", (float)packCellInfo.CellMedian / 1000);
-  commSerial.println();
+  commSerial.printf("=======================================================================================================================================\n");
 }
 
 void hexDump(const char *data, uint32_t dataSize)  //debug function
@@ -372,6 +362,7 @@ void constructBigString()  //debug all data to uart
   snprintf(stringBuffer, STRINGBUFFERSIZE, "CapacityRemain %: %d\n", packBasicInfo.CapacityRemainPercent);
   snprintf(stringBuffer, STRINGBUFFERSIZE, "Temp1: %f\n", (float)packBasicInfo.Temp1 / 10);
   snprintf(stringBuffer, STRINGBUFFERSIZE, "Temp2: %f\n", (float)packBasicInfo.Temp2 / 10);
+  snprintf(stringBuffer, STRINGBUFFERSIZE, "Temp3: %f\n", (float)packBasicInfo.Temp3 / 10);  
   snprintf(stringBuffer, STRINGBUFFERSIZE, "Balance Code Low: 0x%x\n", packBasicInfo.BalanceCodeLow);
   snprintf(stringBuffer, STRINGBUFFERSIZE, "Balance Code High: 0x%x\n", packBasicInfo.BalanceCodeHigh);
   snprintf(stringBuffer, STRINGBUFFERSIZE, "Mosfet Status: 0x%x\n", packBasicInfo.MosfetStatus);
