539bc2d8ca13b063f3e415bba2ee571b3960c4f7
Zodiak1993 <96478454+Zodiak1993@users.noreply.github.com>
2025-11-01 20:29:05 +0100

    Update 6.05.lisp

--- JBD_BMS_BLE_VESC_EXPRESS_BRIDGE/6.05.lisp
+++ JBD_BMS_BLE_VESC_EXPRESS_BRIDGE/6.05.lisp
@@ -1,40 +1,39 @@
-;JBD_BMS_BLE_VESC_BRIDGE:
-;An ESP32 firmware and companion LispBM script
-;to connect multiple JBD BLE BMSs to VESC-EXPRESS via ESPNOW
-;Created: on September 4 2024
-;Author: A-damW, https://github.com/A-damW
+; ===============================
+; ESP-NOW SoC Receiver for VESC
+; VESC 6.05 / 6.06 compatible
+; Receives SoC as string and updates BMS (0.0–1.0)
+; ===============================
 
-; ----------------- Begin user config -----------------
-; Set the total count of cells in your pack
-(def cell-total-count 39)
-; ----------------- End user config -----------------
+(def cell-total-count 16)
+(wifi-set-chan 1)             ; Stelle sicher, dass Sender und Empfänger auf 1 sind
 
-(print (get-mac-addr))
-(print (wifi-get-chan))
-(def cell-index 0)
-(def current-cell 0)
+
+(print "ESP-NOW Bridge Init...")
+(print "MAC:" (get-mac-addr))
+(print "Channel:" (wifi-get-chan))
 
 (esp-now-start)
-
 (set-bms-val 'bms-cell-num cell-total-count)
-;(print "HI")
 
-; Here src is the mac-address of the sender, des is the mac-address
-; of the receiver and data is an array with the sent data. If des is
-; the broadcast address (255 255 255 255 255 255) it means that this
-; was a broadcast packet.
 (defun proc-data (src des data rssi)
-;        (print (list data))
-    (progn
+  (progn
+    ; Direkt den SoC-Wert aus data verwenden
+    (def soc-f (/ (str-to-f data) 100.0))
 
-         (def cell-index (ix (str-split data ":") 0))
-         (def current-cell (ix (str-split data ":") 1))
-         (print cell-index current-cell)
-         (set-bms-val 'bms-v-cell (str-to-i cell-index) (str-to-f current-cell))
-         ;(send-bms-can) ;Un-comment to send bms data over can-bus
+    ; Safety limits
+    (if (< soc-f 0.0) (def soc-f 0.0))
+    (if (> soc-f 1.0) (def soc-f 1.0))
 
-    );progn    
-);proc-data
+    ; Update VESC BMS SoC
+    (set-bms-val 'bms-soc soc-f)
+    (send-bms-can)
+
+    ; Debug output
+    (print "Source:" src "RSSI:" rssi)
+    (print "SoC normalized:" soc-f)
+  )
+)
+
 
 (defun event-handler ()
     (loopwhile t
@@ -48,8 +47,5 @@
     );loopwhile t
 );defun event-handler ()
 
-; Spawn the event handler thread and pass the ID it returns to C
 (event-register-handler (spawn event-handler))
-
-; Enable the custom app data event
 (event-enable 'event-esp-now-rx)
